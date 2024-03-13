eac5cc9797d17c81a5414ebd2aad3a0c07fb80b7
Ishana-Dewmini <ishanadewmini@gmail.com>
2024-03-13 07:55:49 +0530

    create a end point to get all responses of a user

--- Backend/mcq-application/src/main/java/com/energygame/mcqapplication/Controller/ResponseController.java
+++ Backend/mcq-application/src/main/java/com/energygame/mcqapplication/Controller/ResponseController.java
@@ -1,15 +1,15 @@
 package com.energygame.mcqapplication.Controller;
 
 import com.energygame.mcqapplication.Dto.ResponseDto;
+import com.energygame.mcqapplication.Dto.ResponseWithCorrectAnswerDto;
 import com.energygame.mcqapplication.Model.Response;
 import com.energygame.mcqapplication.Service.ResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/responses")
@@ -32,6 +32,15 @@ public class ResponseController {
         }
     }
 
+    @GetMapping("/all/{userId}")
+    public ResponseEntity<List<ResponseWithCorrectAnswerDto>> getAllResponsesForUser(@PathVariable Long userId) {
+        try {
+            List<ResponseWithCorrectAnswerDto> responses = responseService.getAllResponsesForUser(userId);
+            return new ResponseEntity<>(responses, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
 }
--- Backend/mcq-application/src/main/java/com/energygame/mcqapplication/Service/ResponseService.java
+++ Backend/mcq-application/src/main/java/com/energygame/mcqapplication/Service/ResponseService.java
@@ -1,15 +1,20 @@
 package com.energygame.mcqapplication.Service;
 
 import com.energygame.mcqapplication.Dto.ResponseDto;
+import com.energygame.mcqapplication.Dto.ResponseWithCorrectAnswerDto;
 import com.energygame.mcqapplication.Model.Quiz;
 import com.energygame.mcqapplication.Model.Response;
 import com.energygame.mcqapplication.Model.User;
 import com.energygame.mcqapplication.Repository.ResponseRepository;
 import com.energygame.mcqapplication.Repository.UserRepository;
 import com.energygame.mcqapplication.Repository.QuizRepository;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ResponseService {
@@ -40,6 +45,21 @@ public class ResponseService {
         responseRepository.save(response);
     }
 
+    public List<ResponseWithCorrectAnswerDto> getAllResponsesForUser(Long userId) {
+        List<Response> responses = responseRepository.findByUserUserId(userId);
+        List<ResponseWithCorrectAnswerDto> responseDtos = new ArrayList<>();
+
+        for (Response response : responses) {
+            ResponseWithCorrectAnswerDto responseDto = new ResponseWithCorrectAnswerDto();
+            responseDto.setUserId(response.getUser().getUserId());
+            responseDto.setQuestionId(response.getQuestion().getQuestionId());
+            responseDto.setGivenAnswer(response.getGivenAnswer());
+            responseDto.setCorrectAnswer(response.getQuestion().getCorrectAnswer());
+            responseDtos.add(responseDto);
+        }
+
+        return responseDtos;
+    }
 
 
 }
