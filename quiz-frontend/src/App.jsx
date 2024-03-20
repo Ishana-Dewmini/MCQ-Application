043d1827d57f4bff7f3f7ee7b9c29f4ad7dcf101
Rajana Kavinda Samarakoon <91953272+RajanaKavinda@users.noreply.github.com>
2024-03-20 10:05:45 +0530

    Questions were added. v1

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import WelComeQuizComponent from './components/WelComeQuizComponent'
+import Quiz from './components/Quiz'
+import { quizQuestions } from './questions/Questions'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <HeaderComponent />
           <Routes>
             // http://localhost:3000/
-            <Route path="/" element={<WelComeQuizComponent />} />
-          </Routes>      
+            <Route path="/" element={<Quiz questions={quizQuestions.questions} />} />
+          </Routes>   
+      <FooterComponent />       
       </BrowserRouter>
     </>
       
---
+++ quiz-frontend/src/components/Quiz.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import { resultInitalState } from '../questions/Questions';
+
+const Quiz = ({ questions }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [answerIdx, setAnswerIdx] = useState(null)
+  const [answer, setAnswer] = useState(null)
+  const [result, setResult] = useState(resultInitalState)
+  const [showResult, setShowResult] = useState(false)
+
+
+  const { question, choices, correctAnswer } = questions[currentQuestion]
+
+  const onAnswerClick = (answer, index) => {
+    setAnswerIdx(index)
+    if (answer === correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+  }
+
+  const onClickNext = () => {
+    setAnswerIdx(null);
+    setResult((prev) => 
+      answer
+      ? { 
+        ...prev, 
+        score: prev.score + 5 ,
+        correctAnswers: prev.correctAnswers + 1
+      } : {
+        ...prev,
+        wrongAnswers: prev.wrongAnswers + 1
+      }
+      );
+
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+    } 
+    else {
+      setCorrectQuestion(0);
+      setShowResult(true);
+    }
+
+  };
+
+  return (
+    <div className='quiz-container'>
+        <span className='active-question-no'>{currentQuestion + 1 }</span>
+        <span className='total-question'>/{ questions.length }</span>
+        <h2>{ question }</h2>
+        <ul>
+        { 
+          choices.map((choice, index) => (
+                <li
+                  onClick={() => onAnswerClick(choice, index)}
+                  key={choice}
+                  className={answerIdx === index ? "selected-answer" : null}
+                >
+                  {choice}
+                </li>
+              ))}
+        </ul>
+        <div className='center'>
+          <Button variant="contained" color="primary" onClick={onClickNext} disabled = {answerIdx === null}>
+              {currentQuestion == questions.length - 1 ? "Finish" : "Next"}
+          </Button> 
+        </div>
+    
+    </div>
+  )
+}
+
+export default Quiz
