fafb2f8720fbc9d104f6620c148faabcbf15a849
Ishana-Dewmini <ishanadewmini@gmail.com>
2024-03-26 22:34:07 +0530

    update end point url

--- Backend/mcq-application/src/main/java/com/energygame/mcqapplication/Controller/ResponseController.java
+++ Backend/mcq-application/src/main/java/com/energygame/mcqapplication/Controller/ResponseController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/energy-quest")
 public class ResponseController {
 
     @Autowired
