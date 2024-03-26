a9105a8a409c115ca0a370c5123d4eb7bd4d6cf0
Rajana Kavinda Samarakoon <91953272+RajanaKavinda@users.noreply.github.com>
2024-03-26 13:11:50 +0530

    Welcome component updated.

--- quiz-frontend/src/components/Welcome/WelComeQuizComponent.jsx
+++ quiz-frontend/src/components/Welcome/WelComeQuizComponent.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
+import './Welcome.scss';
 
-const WelComeQuizComponent = () => {
+const WelcomeQuizComponent = () => {
   const navigate = useNavigate();
 
   const handleBegin = () => {
-    navigate("/quiz")
+    navigate("/quiz");
   };
 
   return (
     <div>
-      <h1>
+      <h1 className='container1'>
         Welcome to <span>Energy Saving Game Questionnaire</span>
       </h1>
 
-      <button
-        onClick={handleBegin}
-      >
+      <p className='container2'>
+        In this quiz, you'll be presented with 10 multiple-choice questions (MCQs) covering topics such as reducing power consumption, utilizing renewable energy, and participating in demand response programs. Each question is designed to test your knowledge and awareness of these important energy-related concepts.
+        <br />
+        <br />
+        Here's how it works:
+        <br />
+        1. <strong>Questions:</strong> You'll encounter 10 MCQs one by one. Read each question carefully and select the answer you believe is correct. Remember, there's no penalty for wrong answers, so feel free to give it your best shot!
+        <br />
+        2. <strong>Answers and Scores:</strong> After answering all the questions, you'll be able to view your results. You'll see which questions you got right and which ones you got wrong, along with the correct answers. For each correct answer, you'll earn 10 marks.
+        <br />
+        3. <strong>Coins:</strong> The marks you earn in this quiz will be converted into coins in your game environment. These coins can be used for various purposes within the game, helping you progress further and achieve your goals.
+        <br />
+        4. <strong>One Chance Only:</strong> Keep in mind that you only get one chance to take this questionnaire. Once you've completed it, you won't be able to retake it. So make the most of this opportunity to test your knowledge and earn some coins for your game!
+        <br />
+        <br />
+        Are you ready to put your energy knowledge to the test and earn some coins? Let's get started! Good luck! 🌟
+      </p>
+
+      <Button variant="contained" color="success" onClick={handleBegin}>
         Begin Test
-      </button>
+      </Button>
     </div>
   );
-}
+};
 
-export default WelComeQuizComponent
+export default WelcomeQuizComponent;
--- quiz-frontend/src/main.jsx
+++ quiz-frontend/src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.scss'
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css"
+
+// Bootstrap CS
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
