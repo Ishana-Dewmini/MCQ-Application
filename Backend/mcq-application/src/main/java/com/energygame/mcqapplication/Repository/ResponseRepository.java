78e26f0847a4d797fa41d10360f06ac375dfe294
Rajana Kavinda Samarakoon <91953272+RajanaKavinda@users.noreply.github.com>
2024-03-27 11:44:57 +0530

    Delete Backend directory

--- Backend/mcq-application/src/main/java/com/energygame/mcqapplication/Repository/ResponseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energygame.mcqapplication.Repository;
-
-import com.energygame.mcqapplication.Model.Response;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ResponseRepository extends JpaRepository<Response, Long> {
-}
